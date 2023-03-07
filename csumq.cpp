84eef8e80be8b7c436275adc377b71a0d143ad6b
Pedro Schneider <99369967+PedroHenriqueSchneider@users.noreply.github.com>
2023-03-07 19:31:06 -0300

    f

---
+++ csumq.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int numbers_integers, numbers_queries, number, i , j;
+
+    cin >> numbers_integers;
+
+    int numbers[numbers_integers];
+
+    for(int k = 0; k < numbers_integers; k++) {
+        cin >> number;
+        numbers[k] = number;
+    }
+
+    cin >> numbers_queries;
+
+    for(int k = 0; k < numbers_queries; k++) {
+        
+    }
+
+	return 0;
+}
